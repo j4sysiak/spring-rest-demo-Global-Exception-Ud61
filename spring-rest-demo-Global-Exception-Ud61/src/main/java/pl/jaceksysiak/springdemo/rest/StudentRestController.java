a8b8c0c0c289deda12e73785a0ceb7864fc73a82
Jacek <Jacek@BERLIN>
2020-03-15 11:00:29 +0100

    bbb

--- spring-rest-demo-Global-Exception-Ud61/src/main/java/pl/jaceksysiak/springdemo/rest/StudentRestController.java
+++ spring-rest-demo-Global-Exception-Ud61/src/main/java/pl/jaceksysiak/springdemo/rest/StudentRestController.java
@@ -53,30 +53,6 @@ public class StudentRestController {
 		return theStudents.get(studentId);
 	}
 	
-	@ExceptionHandler
-	public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc) {
-		
-		StudentErrorResponse error = new StudentErrorResponse();
-		
-		error.setStatus(HttpStatus.NOT_FOUND.value());
-		error.setMessage(exc.getMessage());
-		error.setTimeStamp(System.currentTimeMillis());
-		
-		return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);  //404
-	 }
-
-	@ExceptionHandler
-	public ResponseEntity<StudentErrorResponse> handleException(Exception exc) {
-		
-		StudentErrorResponse error = new StudentErrorResponse();
-		
-		error.setStatus(HttpStatus.BAD_REQUEST.value());
-		error.setMessage(exc.getMessage());
-		error.setTimeStamp(System.currentTimeMillis());
-		
-		return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);  //400
-	}	
-	
 }
 
 
